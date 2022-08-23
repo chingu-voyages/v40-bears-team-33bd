8bfa62859b0fa8b3b9da4be5267e52a273e8d83a
MrCookiefries <cookiefries12@gmail.com>
2022-08-22 20:25:20 -0500

    add catch all error route handler

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import cors from 'cors'
 import passport from 'passport'
 
@@ -27,6 +27,19 @@ app.use('/api', routes)
 app.use((req, res, next) => {
   const err = new ExpressError(`Endpoint: ${req.path} not found`, 404)
   return next(err)
+})
+
+// catch all error handler
+app.use((err: ExpressError, req: Request, res: Response, next: NextFunction) => {
+  const statusCode = err.statusCode || 500
+  const message = err.message || 'Something went wrong'
+
+  // tests will error on purpose, don't log during tests
+  if (process.env.NODE_ENV !== 'test') {
+    console.error(err.stack)
+  }
+
+  return res.status(statusCode).json({ error: { statusCode, message } })
 })
 
 const server = app.listen(config.appPort, () => {
